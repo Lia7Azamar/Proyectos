bf03bd0939321c85267c7d9742e20acb2158095d
Lia7Azamar <162234357+Lia7Azamar@users.noreply.github.com>
2024-05-14 14:14:18 -0600

    Add files via upload

---
+++ tercerapartecopi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class tercerapartecopi {
+    public int terceraparte( ){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Dame un numero entre 50 y 200");
+        float p,c;
+        int x,d;
+        x=sc.nextShort();
+        d=x*2;
+        p=d/3;
+        c=p/2;
+        System.out.println("La mitad de la tercera parte del doble de "+x+"-es-"+c);
+    }
+}
